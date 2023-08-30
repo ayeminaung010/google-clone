844c01c3ff1516f71163c7f1ceebe13a6593cd07
yasuoOwO <heinmina05@gmail.com>
2023-08-30 15:14:37 +0630

    fetch data from google

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+// https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
+const WebSearchPage = async({searchParams}) => {
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams?.search}`)
+  const data = await response.json()
+  const results = data.items;
 
-const WebSearchPage = () => {
+
   return (
-    <div>
-        WebSearchPage
-    </div>
+    <>
+        {
+          results &&  results.map((result) => (
+            <h1 key={result}>{result?.title}</h1>
+          ))
+        }
+    </> 
   )
 }
 
--- src/components/SearchHeaderOption.jsx
+++ src/components/SearchHeaderOption.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const SearchHeaderOption = () => {
-  return (
-    <div>
-        SearchHeaderOption
-    </div>
-  )
-}
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParam = useSearchParams();
+  const searchTerm = searchParam.get("search");
+  const selectTab = (tab) => {
+    router.push(
+      `/search/${tab === "Images" ? "image" : "web"}?search=${searchTerm}`
+    );
+  };
 
-export default SearchHeaderOption
+  return (
+    <div className=" flex space-x-2 select-none border-b-4 w-full justify-center lg:justify-start lg:pl-52 text-gray-700 text-sm">
+      <div
+        onClick={() => selectTab("All")}
+        className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500  cursor-pointer pb-3 px-2 
+      ${pathname === "/search/web" && "!text-blue-600  !border-blue-600 "}
+      `}
+      >
+        <AiOutlineSearch className=" text-md " />
+        <p>All</p>
+      </div>
+      <div
+        onClick={() => selectTab("Images")}
+        className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500  cursor-pointer pb-3 px-2 
+      ${pathname === "/search/image" && "!text-blue-600  !border-blue-600 "}
+      `}
+      >
+        <AiOutlineSearch className=" text-md " />
+        <p>Images</p>
+      </div>
+    </div>
+  );
+};
+
+export default SearchHeaderOption;
